8ae6fd199a7b9c1052c4e009122c9e24bba1da89
Nath A. <natiamador789@gmail.com>
2025-06-16 21:15:03 -0600

    Revert "Semana 6"
    This reverts commit 0ac332c1e1dd61b32e1a50587a11c3dfbe628736.

--- src/main/java/com/tienda/domain/Categoria.java
+++ src/main/java/com/tienda/domain/Categoria.java
@@ -1,62 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package com.tienda.domain;
 
 import jakarta.persistence.*;
 import java.io.Serializable;
+import lombok.Data;
 
+/**
+ *
+ * @author natia
+ */
+@Data
 @Entity
 @Table(name = "categoria")
 public class Categoria implements Serializable {
 
-    private static final long serialVersionUID = 1L;
+    //en camelCase se llaman asi, pero en la bd no realmente, so what do we do?
+    //nath de 10 minutos despues: ponemos los arrobas para que tengan las mismas propiedades que en mySQL
+    private static final long serialVersionUID = 1L;//establecer a quien estamos mandando a base de datos
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_categoria")
     private Long idCategoria;
-
     private String descripcion;
-
     @Column(name = "ruta_imagen")
     private String rutaImagen;
-
     private boolean activo;
 
-    public Categoria() {}
+    public Categoria() {
+    }
 
     public Categoria(String descripcion, boolean activo) {
         this.descripcion = descripcion;
         this.activo = activo;
     }
-
-    public Long getIdCategoria() {
-        return idCategoria;
-    }
-
-    public void setIdCategoria(Long idCategoria) {
-        this.idCategoria = idCategoria;
-    }
-
-    public String getDescripcion() {
-        return descripcion;
-    }
-
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
-    }
-
-    public String getRutaImagen() {
-        return rutaImagen;
-    }
-
-    public void setRutaImagen(String rutaImagen) {
-        this.rutaImagen = rutaImagen;
-    }
-
+    
     public boolean isActivo() {
-        return activo;
-    }
+    return Boolean.TRUE.equals(this.activo);
+}
 
-    public void setActivo(boolean activo) {
-        this.activo = activo;
-    }
+    
+    
+    
+
 }
--- src/main/java/com/tienda/service/impl/CategoriaServiceImpl.java
+++ src/main/java/com/tienda/service/impl/CategoriaServiceImpl.java
@@ -1,8 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.tienda.service;
+package com.tienda.service.impl;
 
 import com.tienda.dao.CategoriaDao;
 import com.tienda.domain.Categoria;
@@ -14,35 +10,18 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class CategoriaServiceImpl implements CategoriaService {
-
     @Autowired
     private CategoriaDao categoriaDao;
-
+     
+    
     @Override
     @Transactional(readOnly = true)
-    public List<Categoria> getCategorias(boolean activos) {
-        var lista = categoriaDao.findAll();
-        if (activos) {
+    public List<Categoria> getCategorias(boolean activos){
+        var lista= categoriaDao.findAll();
+        if(activos){
             lista.removeIf(e -> !e.isActivo());
         }
         return lista;
-    }
-
-    @Override
-    @Transactional(readOnly = true)
-    public Categoria getCategoria(Categoria categoria) {
-        return categoriaDao.findById(categoria.getIdCategoria()).orElse(null);
-    }
-
-    @Override
-    @Transactional
-    public void save(Categoria categoria) {
-        categoriaDao.save(categoria);
-    }
-
-    @Override
-    @Transactional
-    public void delete(Categoria categoria) {
-        categoriaDao.delete(categoria);
-    }
+                }
+    
 }
