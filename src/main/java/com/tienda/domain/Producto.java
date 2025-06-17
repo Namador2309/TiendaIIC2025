0ac332c1e1dd61b32e1a50587a11c3dfbe628736
Nath A. <natiamador789@gmail.com>
2025-06-16 21:10:50 -0600

    Semana 6

--- src/main/java/com/tienda/domain/Producto.java
+++ src/main/java/com/tienda/domain/Producto.java
@@ -4,36 +4,52 @@ import jakarta.persistence.*;
 import java.io.Serializable;
 
 @Entity
-@Table(name = "categoria")
-public class Categoria implements Serializable {
+@Table(name = "producto")
+public class Producto implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_producto")
+    private Long idProducto;
     @Column(name = "id_categoria")
     private Long idCategoria;
-
+    private String detalle;
     private String descripcion;
-
+    private double precio;
+    private int existencias;
     @Column(name = "ruta_imagen")
     private String rutaImagen;
-
     private boolean activo;
 
-    public Categoria() {}
+    public Producto() {
+    }
 
-    public Categoria(String descripcion, boolean activo) {
+    public Producto(String descripcion, boolean activo) {
         this.descripcion = descripcion;
         this.activo = activo;
     }
 
-    public Long getIdCategoria() {
-        return idCategoria;
+    public Producto(Long idProducto, Long idCategoria, String detalle, String descripcion, double precio, int existencias, String rutaImagen, boolean activo) {
+        this.idProducto = idProducto;
+        this.idCategoria = idCategoria;
+        this.detalle = detalle;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.existencias = existencias;
+        this.rutaImagen = rutaImagen;
+        this.activo = activo;
+    }
+    
+    
+
+    public Long getIdProducto() {
+        return idProducto;
     }
 
-    public void setIdCategoria(Long idCategoria) {
-        this.idCategoria = idCategoria;
+    public void setIdProducto(Long idProducto) {
+        this.idProducto = idProducto;
     }
 
     public String getDescripcion() {
